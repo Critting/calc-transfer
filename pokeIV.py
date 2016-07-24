f7883e6528b16bcbf30c96db7d9c2ecd64eecdff
emmental <imoutits@gmail.com>
2016-07-23 21:49:12 -0700

    massively cleaned up the code -- moved a lot to pokemondata

--- pokeIV.py
+++ pokeIV.py
@@ -13,6 +13,8 @@ import getpass
 import csv
 import time
 
+from pokemondata import PokemonData
+
 sys.path.insert(0, './pogo')
 from custom_exceptions import GeneralPogoException
 
@@ -21,19 +23,6 @@ from location import Location
 
 # add directory of this file to PATH, so that the package will be found
 sys.path.append(os.path.dirname(os.path.realpath(__file__)))
-
-# -- global dictionaries for pokedex, families, and evolution prices
-with open('names.tsv') as f:
-    f.readline()
-    POKEDEX = dict(csv.reader(f, delimiter='\t'))
-    
-with open('families.tsv') as f:
-    f.readline()
-    FAMILIES = dict(csv.reader(f, delimiter='\t'))    
-    
-with open('evolves.tsv') as f:
-    f.readline()
-    COSTS = dict(csv.reader(f, delimiter='\t'))
 
 def setupLogger():
     logger = logging.getLogger()
@@ -97,96 +86,6 @@ def init_config():
 
     return config
 
-def get_needed_counts(pokemon, uniques, evolves):
-    needed = dict()
-    for p in pokemon:
-        if str(p.number) in evolves and str(p.number) in uniques:
-           needed[str(p.number)] = evolves[str(p.number)] - uniques[str(p.number)]
-    return needed
-
-def get_unique_counts(pokemon):
-    uniques = dict()
-    for p in pokemon:
-        if (str(p.number) == str(p.family)):
-           if str(p.number) in uniques:
-                uniques[str(p.number)] = uniques[str(p.number)] + 1
-           else:
-                uniques[str(p.number)] = 1
-    return uniques
-				
-def get_evolve_counts(pokemon, evolve_list):
-    evolves = dict()
-    total = 0
-    
-    if evolve_list is not None:
-        evolve_list = [x.lower() for x in evolve_list]
-    
-    for p in pokemon:
-        if evolve_list is not None and str(p.number) not in evolve_list and p.name.lower() not in evolve_list:
-            continue
-        if str(p.number) == str(p.family) and str(p.number) not in evolves and hasattr(p,'cost'):
-            if int(p.candy/p.cost) > 0:
-                evolves[str(p.number)] = int(p.candy/p.cost)
-                total += int(p.candy/p.cost)
-    evolves["total"] = total
-    return evolves
-
-def get_pokemon(pokemon, candies):
-    data = []
-    
-    for p in pokemon:
-        pok = type('',(),{})
-        pok.id = p.id
-        pok.number = p.pokemon_id
-        pok.name = POKEDEX[str(pok.number)]
-        pok.family = FAMILIES[str(pok.number)]
-        pok.stamina = int(p.individual_stamina) if hasattr(p,"individual_stamina") else 0
-        pok.attack = int(p.individual_attack) if hasattr(p,"individual_attack") else 0
-        pok.defense = int(p.individual_defense) if hasattr(p,"individual_defense") else 0
-        pok.iv = ((pok.stamina + pok.attack + pok.defense) / float(45))*100
-        pok.ivPercent = pok.iv/100
-        pok.cp = p.cp
-        if int(COSTS[str(pok.number)]) > 0:
-            pok.cost = int(COSTS[str(pok.number)])
-        pok.candy = candies[int(pok.family)]
-        data.append(pok)
-    return data
-
-def get_above_iv(pokemon, ivmin):
-    if len(pokemon) == 0:
-        return []
-    best = []
-    
-    #sort by iv
-    pokemon.sort(key=lambda x: x.iv, reverse=True)
-    for p in pokemon:
-        #if it passes the minimum iv test
-        if p.iv >= float(ivmin):			
-            best.append(p)
-
-    return best
-
-def get_best_pokemon(pokemon, ivmin, cpmin):
-    if len(pokemon) == 0:
-        return []
-
-    best = []
-    
-    #sort by iv
-    pokemon.sort(key=lambda x: x.iv, reverse=True)
-    for p in pokemon:
-        #if there isn't a pokemon in best with the same number (name) as this one, add it
-        if not any(x.number == p.number for x in best):
-            best.append(p)
-        #if it passes the minimum iv test
-        elif p.iv >= float(ivmin):
-            best.append(p)
-        #if cp_override is set, check CP
-        elif cpmin > 0 and int(p.cp) >= int(cpmin):
-            best.append(p)
-
-    return best
-
 def print_header(title):
     print('{0:<15} {1:^20} {2:>15}'.format('------------',title,'------------'))
 
@@ -205,6 +104,48 @@ def print_pokemon_verbose(pokemon):
     print('{0:<10} {1:>6} {2:>6} {3:>6} {4:>8} {5:>8}'.format('[POKEMON]','[ATK]','[DEF]','[STA]','[CP]','[IV]'))
     for p in pokemon:
         print('{0:<10} {1:>6} {2:>6} {3:>6} {4:>8} {5:>8.2%}'.format(str(p.name),str(p.attack),str(p.defense),str(p.stamina),str(p.cp),p.ivPercent))
+
+def print_evolve_candidates(data):
+    if data["evolve"]:
+        print_header('Available evolutions')
+        print_header('TOTAL: '+str(data["evolve_counts"]["total"])+' / '+data["config"].max_evolutions)
+        print('{0:<10} {1:<15} {2:<17} {3:>10}'.format('[pokemon]','[# evolutions]','[# in inventory]','[# needed]'))
+        for id in list(data["evolve_counts"].keys()):
+            if id in data["needed_counts"] and id in data["unique_counts"] and data["needed_counts"][id] <= 0:
+                print('{0:<10} {1:^15} {2:^17} {3:^10}'.format(data["pokedex"][id],data["evolve_counts"][id],data["unique_counts"][id],""))
+            elif id in data["needed_counts"] and id in data["unique_counts"]:
+                print('{0:<10} {1:^15} {2:^17} {3:^10}'.format(data["pokedex"][id],data["evolve_counts"][id],data["unique_counts"][id],data["needed_counts"][id]))
+
+def transfer_pokemon(data, session):
+    if data["config"].transfer and data["transfer"]:
+        print('{0:<15} {1:^20} {2:>15}'.format('------------','Transferring','------------'))
+        for p in data["transfer"][:]:
+            id = str(p.number)
+            logging.info('{0:<35} {1:<8} {2:<8.2%}'.format('transferring pokemon: '+str(p.name),str(p.cp),p.ivPercent,))
+            session.releasePokemon(p)
+            if id in list(data["unique_counts"].keys()):
+                data["unique_counts"][id] = data["unique_counts"][id] - 1 #we now have one fewer of these...
+            if p in data["transfer"]:
+                data["transfer"].remove(p)
+            if p in data["all"]:
+                data["all"].remove(p)
+            time.sleep(int(data["config"].transfer_delay))
+
+def evolve_pokemon(data, session):
+    if data["config"].evolve and data["evolve"]:
+        for p in data["evolve"][:]:
+            id = str(p.number)
+            logging.info('{0:<35} {1:<8} {2:<8.2%}'.format('evolving pokemon: '+str(p.name),str(p.cp),p.ivPercent))
+            session.evolvePokemon(p)
+            data["evolve_counts"][id] = data["evolve_counts"][id] - 1
+            data["unique_counts"][id] = data["unique_counts"][id] - 1
+            if p in data["evolve"]:
+                data["evolve"].remove(p)
+            if p in data["all"]:
+                data["all"].remove(p)
+            if p in data["extra"]:
+                data["extra"].remove(p)
+            time.sleep(int(data["config"].evolution_delay))
 
 def main():
     setupLogger()
@@ -237,101 +178,46 @@ def main():
     pokemon = inventory["party"]
     candy = inventory["candies"]
     
-    # add candy to pokemon and reformat the list
-    pokemon = get_pokemon(pokemon, candy)
+    # -- dictionaries for pokedex, families, and evolution prices
+    with open('names.tsv') as f:
+        f.readline()
+        pokedex = dict(csv.reader(f, delimiter='\t'))
+        
+    with open('families.tsv') as f:
+        f.readline()
+        family = dict(csv.reader(f, delimiter='\t'))    
+        
+    with open('evolves.tsv') as f:
+        f.readline()
+        cost = dict(csv.reader(f, delimiter='\t'))
     
-    if len(pokemon) == 0:
+    data = PokemonData(pokemon, candy, pokedex, family, cost, config)
+    
+    if len(data["all"]) == 0:
         print('You have no pokemon...')
         return
-    # highest IV pokemon
-    best = []
-    if config.hard_minimum:
-        best = get_above_iv(pokemon, float(config.minimumIV))
-    else:
-        mincp = int(config.cp_override) if config.cp_override is not None else 0
-        best = get_best_pokemon(pokemon, float(config.minimumIV), mincp)
-    # rest of pokemon
-    extras = list(set(pokemon) - set(best))
-    others = []
-    transfers = []
-    evolutions = []
-    # evolution information
-    uniques = get_unique_counts(pokemon)
-    evolves = get_evolve_counts(pokemon, config.evolve_list)
-    needed = get_needed_counts(pokemon, uniques, evolves)    
-    #------- get transfers and other
-    if extras:
-        extras.sort(key=lambda x: x.iv)
-        used = dict()
-        for p in extras:
-            id = str(p.number)
-            used[id] = 0 if id not in used else used[id]
-            if id not in evolves.keys() or used[id] < (uniques[id] - evolves[id]):
-                transfers.append(p)
-            else:
-                others.append(p)
-            used[id] = used[id] + 1
-    others.sort(key=lambda x: x.iv, reverse=True)
-    transfers.sort(key=lambda x: x.iv)
-    #------- get pokemon to be evolved
-    if any(evolves):
-        pokemon.sort(key=lambda x: x.iv, reverse=True)
-        count = dict()
-        for p in pokemon:
-            id = str(p.number)
-            count[id] = 0 if id not in count else count[id]
-            if id in evolves.keys() and count[id] < int(evolves[id]):
-                evolutions.append(p)
-                count[id] = count[id] + 1
+    
     #------- best pokemon
-    if best:
+    if data["best"]:
         print_header('Highest IV Pokemon')
-        print_pokemon(best, config.verbose)
+        print_pokemon(data["best"], data["config"].verbose)
     #------- transferable pokemon
-    if transfers:
+    if data["transfer"]:
         print_header('May be transfered')
-        print_pokemon(transfers, config.verbose)
+        print_pokemon(data["transfer"], data["config"].verbose)
     #------- extras that aren't to be transfered
-    if others:
+    if data["other"]:
         print_header('Other Pokemon')
-        print_pokemon(others, config.verbose)
+        print_pokemon(data["other"], data["config"].verbose)
     #------- evolve candidate  pokemon
-    if evolutions:
-        print_header('Available evolutions')
-        print_header('TOTAL: '+str(evolves["total"])+' / '+config.max_evolutions)
-        print('{0:<10} {1:<15} {2:<17} {3:>10}'.format('[pokemon]','[# evolutions]','[# in inventory]','[# needed]'))
-        for id in list(evolves.keys()):
-            if id in needed.keys() and id in uniques.keys() and needed[id] <= 0:
-                print('{0:<10} {1:^15} {2:^17} {3:^10}'.format(POKEDEX[id],evolves[id],uniques[id],""))
-            elif id in needed.keys() and id in uniques.keys():
-                print('{0:<10} {1:^15} {2:^17} {3:^10}'.format(POKEDEX[id],evolves[id],uniques[id],needed[id]))
+    if data["evolve"]:
+        print_evolve_candidates(data)
     #------- transfer extra pokemon
-    if config.transfer and transfers:
-        print('{0:<15} {1:^20} {2:>15}'.format('------------','Transferring','------------'))
-        for p in transfers[:]:
-            logging.info('{0:<35} {1:<8} {2:<8.2%}'.format('transferring pokemon: '+str(p.name),str(p.cp),p.ivPercent,))
-            session.releasePokemon(p)
-            if id in uniques.keys():
-                uniques[id] = uniques[id] - 1 #we now have one fewer of these...
-            if p in transfers:
-                transfers.remove(p)
-            if p in pokemon:
-                pokemon.remove(p)
-            time.sleep(int(config.transfer_delay))
+    if data["config"].transfer and data["transfer"]:
+        transfer_pokemon(data, session)
     #------- evolving t1 pokemon
-    if config.evolve and evolutions:
-        for p in evolutions[:]:
-            logging.info('{0:<35} {1:<8} {2:<8.2%}'.format('evolving pokemon: '+str(p.name),str(p.cp),p.ivPercent))
-            session.evolvePokemon(p)
-            evolves[id] = evolves[id] - 1
-            uniques[id] = uniques[id] - 1
-            if p in evolutions:
-                evolutions.remove(p)
-            if p in pokemon:
-                pokemon.remove(p)
-            if p in extras:
-                extras.remove(p)
-            time.sleep(int(config.evolution_delay))
+    if data["config"].evolve and data["evolve"]:
+        evolve_pokemon(data, session)
     
 if __name__ == '__main__':
     main()
---
+++ pokemondata.py
@@ -0,0 +1,135 @@
+class PokemonData(dict):
+    #A dictionary for all of the key information used in pokeIV
+    
+    def set_all(self, pokemon):
+        self["all"] = []
+        
+        for p in pokemon:
+            pok = type('',(),{})
+            pok.id = p.id
+            pok.number = p.pokemon_id
+            pok.name = self["pokedex"][str(pok.number)]
+            pok.family = self["family"][str(pok.number)]
+            pok.stamina = int(p.individual_stamina) if hasattr(p,"individual_stamina") else 0
+            pok.attack = int(p.individual_attack) if hasattr(p,"individual_attack") else 0
+            pok.defense = int(p.individual_defense) if hasattr(p,"individual_defense") else 0
+            pok.iv = ((pok.stamina + pok.attack + pok.defense) / float(45))*100
+            pok.ivPercent = pok.iv/100
+            pok.cp = p.cp
+            if int(self["cost"][str(pok.number)]) > 0:
+                pok.cost = int(self["cost"][str(pok.number)])
+            pok.candy = self["candy"][int(pok.family)]
+            self["all"].append(pok)
+
+        self["all"].sort(key=lambda x: x.iv, reverse=True)
+
+    def set_best(self):
+        if len(self["all"]) == 0:
+            return []
+        self["best"] = []
+
+        for p in self["all"]:
+            #if there isn't a pokemon in best with the same number (name) as this one, add it
+            if not any(x.number == p.number for x in self["best"]):
+                self["best"].append(p)
+            #if it passes the minimum iv test
+            elif p.iv >= float(self["config"].minimumIV):
+                self["best"].append(p)
+            #if cp_override is set, check CP
+            elif self["config"].cp_override is not None and self["config"].cp_override > 0 and int(p.cp) >= int(self["config"].cp_override):
+                self["best"].append(p)
+
+        self["best"].sort(key=lambda x: x.iv, reverse=True)
+		
+    def set_transfer(self):
+        self["transfer"] = []
+        if self["extra"]:
+            used = dict()
+            for p in self["extra"]:
+                id = str(p.number)
+                used[id] = 0 if id not in used else used[id]
+                if id not in self["evolve_counts"] or used[id] < (self["unique_counts"][id] - self["evolve_counts"][id]):
+                    self["transfer"].append(p)
+                used[id] = used[id] + 1
+
+        self["transfer"].sort(key=lambda x: x.iv)
+
+    def set_evolve(self):
+        self["evolve"] = []
+        if any(self["evolve_counts"]):
+            count = dict()
+            for p in self["all"]:
+                id = str(p.number)
+                count[id] = 0 if id not in count else count[id]
+                if id in self["evolve_counts"] and count[id] < int(self["evolve_counts"][id]):
+                    self["evolve"].append(p)
+                    count[id] = count[id] + 1
+        self["evolve"].sort(key=lambda x: x.iv, reverse=True)
+        
+    def set_unique_counts(self):
+        self["unique_counts"] = dict()
+        for p in self["all"]:
+            if (str(p.number) == str(p.family)):
+                if str(p.number) in self["unique_counts"]:
+                    self["unique_counts"][str(p.number)] = self["unique_counts"][str(p.number)] + 1
+                else:
+                    self["unique_counts"][str(p.number)] = 1
+
+    def set_evolve_counts(self):
+        self["evolve_counts"] = dict()
+        total = 0
+
+        if self["config"].evolve_list is not None:
+            self["config"].evolve_list = [x.lower() for x in self["config"].evolve_list]
+
+        for p in self["all"]:
+            if self["config"].evolve_list is not None and str(p.number) not in self["config"].evolve_list and p.name.lower() not in self["config"].evolve_list:
+                continue
+            if str(p.number) == str(p.family) and str(p.number) not in self["evolve_counts"] and hasattr(p,'cost'):
+                if int(p.candy/p.cost) > 0:
+                    self["evolve_counts"][str(p.number)] = int(p.candy/p.cost)
+                    total += int(p.candy/p.cost)
+        self["evolve_counts"]["total"] = total
+
+    def set_needed_counts(self):
+        self["needed_counts"] = dict()
+        for p in self["all"]:
+            if str(p.number) in self["evolve_counts"] and str(p.number) in self["unique_counts"]:
+                self["needed_counts"][str(p.number)] = self["evolve_counts"][str(p.number)] - self["unique_counts"][str(p.number)]
+
+    def set_top(self):
+        if len(self["all"]) == 0:
+            return []
+        self["best"] = []
+
+        for p in self["all"]:
+            #if it passes the minimum iv test
+            if p.iv >= float(self["config"].minimumIV):			
+                self["best"].append(p)
+
+        self["best"].sort(key=lambda x: x.iv, reverse=True)
+	
+    #takes a list of pokemon from the API, 
+    #a candies dict from the API,
+    #a pokedex dict {number,name}, 
+    #a family dict {number, family}, 
+    #an evolve cost dict {number, cost},
+    #and the configuration options
+    def __init__(self, pokemon, candies, pokedex, family, cost, config):
+        self["candy"] = candies
+        self["pokedex"] = pokedex
+        self["family"] = family
+        self["cost"] = cost
+        self["config"] = config
+        self.set_all(pokemon)
+        if self["config"].hard_minimum:
+            self.set_top()
+        else:
+            self.set_best()
+        self.set_evolve_counts()
+        self.set_unique_counts()
+        self.set_needed_counts()
+        self["extra"] = sorted(list(set(self["all"]) - set(self["best"])), key=lambda x: x.iv)
+        self.set_transfer()
+        self["other"] = sorted(list(set(self["extra"]) - set(self["transfer"])), key=lambda x: x.iv, reverse=True)
+        self.set_evolve()
